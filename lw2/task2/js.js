3d24d44069d2daa93f6e3c2a1a9212411085c86e
Fantasy <dmitriypet03@bk.ru>
2024-02-20 19:14:19 +0300

    lw2: task2 - изменение цвета, экспорт, рисование линий

--- lw2/task2/js.js
+++ lw2/task2/js.js
@@ -1,36 +1,76 @@
+const PIXEL_SIZE = 8
+
+function getCanvasContext() {
+    const canvas = document.getElementById('canvas')
+    return canvas?.getContext('2d')
+}
+
+function drawPixel(context, x, y) {
+    const colorInput = document.getElementById('color')
+    context.fillStyle = colorInput.value
+    context.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE)
+}
+
 function onChangeFile() {
-    const input = document.getElementById("file").files[0]
-    const image = document.getElementById('image')
+    const input = document.getElementById('file').files[0]
+    const image = document.getElementById('div-image')
 
-    console.log('changed')
-
-    input.value = ""
+    input.value = ''
 
     const reader = new FileReader()
     reader.readAsDataURL(input)
 
     reader.onload = () => {
-        image.src = reader.result
-        image.style['opacity'] = '1'
-    };
+        image.style['background-image'] = `url(${reader.result})`
+    }
     reader.onerror = () => {
         alert('Не удалось загрузить файл')
     }
 }
 
 function onClickButtonNew() {
-    const image = document.getElementById('image')
+    const input = document.getElementById('file')
+    input.value = ''
+    const image = document.getElementById('div-image')
+    image.style['background-image'] = 'none'
 
-    image.style['opacity'] = '0'
+    const canvas = document.getElementById('canvas')
+    const context = getCanvasContext()
+    context.clearRect(0, 0, canvas.width, canvas.height)
 }
 
 function onClickButtonOpen() {
-    const input = document.getElementById("file")
+    const input = document.getElementById('file')
     input.click()
 }
 
+function onClickButtonSave(format) {
+    const image = document.getElementById('div-image') 
+
+    html2canvas(image).then(function(canvas) {
+        canvas.toBlob(function(blob) {
+            saveAs(blob, `image.${format}`)
+        });
+    });
+}
+
+function onMouseMoveHandler(context, e) {
+    drawPixel(context, e.clientX - 12, e.clientY - 69)
+}
+
 function main() {
-    
+    const context = getCanvasContext()
+
+    function onMouseMove(e) {
+        onMouseMoveHandler(context, e)
+    }
+
+    document.addEventListener('mousedown', () => {
+        document.addEventListener('mousemove', onMouseMove)
+    })
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', onMouseMove)
+    })
 }
 
 main()
